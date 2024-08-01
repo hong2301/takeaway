b2fee25201969446baaedc7e17405d97678f18c1
hong <lizefengooo@qq.com>
2024-08-01 16:56:36 +0800

    chore(全局):创建初始项目
    uniapp+ts+pinia+vite+uni-ui的环境搭建

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <view class="viewport">
+   肯德基
+  </view>
+</template>
+
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ src/types/member.d.ts
@@ -0,0 +1,6 @@
+
+/** 登录用户信息 */
+export type LoginResult = {
+  /** 登录凭证 */
+  token: string
+}
