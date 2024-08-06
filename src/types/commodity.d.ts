62bb5f91a5625aa0ce7a428a368667f5b09cd120
hong <lizefengooo@qq.com>
2024-08-06 16:00:46 +0800

    style(项目结构)声明了菜单商品等类型

--- src/types/commodity.d.ts
+++ src/types/commodity.d.ts
@@ -1,4 +1,14 @@
 /** 菜单信息 */
-export type restaurant = {
-  
+export type Menu = Array<MenuItem>
+
+export type MenuItem = {
+  icon: string
+  name: string
+  spus: Array<Spu>
+}
+
+export type Spu = {
+  picture: string
+  name: string
+  min_price: number
 }
